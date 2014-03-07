3814c0c591b45cdfdaf0d941a716d1b1c38bed95
user <none@none>
2014-03-08 07:50:38 +1100

    No More JSON for ii-txt

--- .py/reparse.py
+++ .py/reparse.py
@@ -1,12 +1,24 @@
 # -*- coding: utf-8 -*-
 
-import json, codecs, os
+import codecs, os
 
 def get_echoarea(name):
     try:
         return open('echo/%s' % name).read().splitlines()
     except:
         return ''
+
+def _parz(msg):
+    pz = msg.splitlines()
+    mo = dict()
+    optz = pz[0].split('/')
+    mo.update( dict(zip(optz[::2],optz[1::2])) )
+    for i,n in enumerate(('echoarea','date','msgfrom','addr','msgto','subj'),1):
+        mo[n] = pz[i]
+    mo['msg'] = '\n'.join(pz[8:])
+    mo['date'] = int(mo['date'])
+    return mo
+
 
 cfg = open('../config.cfg').read().splitlines()
 
@@ -16,7 +28,7 @@ for ea in cfg[2:]:
     my = get_echoarea(ea) or []
     f = codecs.open('../%s/0000.txt' % ea,'w','utf-8')
     for i,m in enumerate(my,1):
-        mo = json.loads(open('msg/%s' % m).read())
+        mo = _parz(codecs.open('msg/%s' % m,'r','utf-8').read())
         buf = m + '\n' + mo['msgfrom'] + '\naddr: ' + str(mo['addr']) + '\nmsgto: ' + mo['msgto'] + '\n' + mo['subj'] + '\n\n' + mo['msg']
         codecs.open('../%s/%s.txt' % (ea,i),'w','utf-8').write(buf)
         f.write('== %s ========================= ' % i + buf + '\n\n\n')

c59f3d8c032aefb2e37a5bf8a98aae2c82f2bf04
Vasiliy Vylegzhanin <v.gadfly@gmail.com>
2014-06-13 18:24:13 +0400

    Fixed newmsg.py crash on empty messages.

--- .py/newmsg.py
+++ .py/newmsg.py
@@ -5,6 +5,8 @@ import codecs, os
 def _parz(msg):
     pz = msg.splitlines()
     mo = dict()
+    if len(pz) < 8: # bad mesg
+        return None
     optz = pz[0].split('/')
     mo.update( dict(zip(optz[::2],optz[1::2])) )
     for i,n in enumerate(('echoarea','date','msgfrom','addr','msgto','subj'),1):
@@ -15,7 +17,9 @@ def _parz(msg):
 
 f = codecs.open('../newmsg.txt','w','utf-8')
 for m in open('.newmsg').read().splitlines():
+    print ("%s\n" % m)
     mo = _parz(codecs.open('msg/%s' % m,'r','utf-8').read())
-    buf = m + '\n' + mo['msgfrom'] + ' (' + str(mo['addr']) + ')\nmsgto: ' + mo['msgto'] + '\n' + mo['subj'] + '\n\n' + mo['msg']
-    f.write('== %s ========================= ' % mo['echoarea'] + buf + '\n\n\n')
+    if mo != None:
+        buf = m + '\n' + mo['msgfrom'] + ' (' + str(mo['addr']) + ')\nmsgto: ' + mo['msgto'] + '\n' + mo['subj'] + '\n\n' + mo['msg']
+        f.write('== %s ========================= ' % mo['echoarea'] + buf + '\n\n\n')
 f.close()
